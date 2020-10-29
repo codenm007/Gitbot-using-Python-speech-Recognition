e49958aee054f3f1dfe7b8dffe20646ebfe1f62b
NILANJAN MAJUMDAR <n.majumder14@gmail.com>
2020-10-29 13:56:03 +0530

    committed by viki the gitbot

--- main.py
+++ main.py
@@ -10,48 +10,47 @@ print (rate)                        #printing current voice rate
 engine.setProperty('rate', 159)     # setting up new voice rate
 voices = engine.getProperty('voices')
 
-
 #reading json files
 with open('readout.json') as read_out:
     read_out_data = json.load(read_out)
-#reading dictionaries
-from reverse_search_dict import test_dict
-
-
-
-def comprehension(read_dic):
-    results = [k for k,v in test_dict.items() if read_dic in v]
-    return results
-
-
-print(comprehension("name"))
+with open('comprehensions.json') as comprehensions:
+    comprehensions_data = json.load(comprehensions)
 
 #function to execute
 def exec_command(voice_data):
-    meaning = comprehension(voice_data)
-    print(meaning)
-    if meaning == ['what is your name']:
+    if there_exists(comprehensions_data["what is your name"],voice_data):
         engine.say(read_out_data["name"])
         engine.runAndWait()
-    if meaning == ['time']:
-        engine.say(datetime.now())
+    if there_exists(comprehensions_data["time"],voice_data):
+        engine.say("The time is:")
+        engine.say(datetime.now().strftime("%I:%M %p"))
         engine.runAndWait()
-    if voice_data == "backup":
+    if there_exists(comprehensions_data["backup my code"],voice_data):
         os.system("git add .")
         engine.say(read_out_data["git add"])
         engine.runAndWait()
-    if meaning == ['commit my code']:
-        engine.say(read_out_data["sing gariwala"])
+    if there_exists(comprehensions_data["commit my code"],voice_data):
+        os.system('git commit -m "committed by viki the gitbot"')
+        engine.say(read_out_data["commit"])
         engine.runAndWait()
-    if meaning == ['upload to cloud']:
-        engine.say(read_out_data["sing gariwala"])
+    if there_exists(comprehensions_data["push to remote repo"],voice_data):
+        os.system("git push origin master")
+        engine.say(read_out_data["remote_repo"])
         engine.runAndWait()
-    if meaning == ['tell me about yourself']:
-        engine.say(read_out_data["sing gariwala"])
+    if there_exists(comprehensions_data["about"],voice_data):
+        engine.say(read_out_data["about"])
         engine.runAndWait()
-    if meaning == ['update yourself']:
-        engine.say(read_out_data["sing gariwala"])
+    if there_exists(comprehensions_data["what is your name"],voice_data):
+        os.system("git pull origin master")
+        engine.say(read_out_data["pull"])
         engine.runAndWait()
+
+
+#function to match if anything is matching
+def there_exists(terms,voice_data):
+    for term in terms:
+        if term in voice_data:
+            return True
 
 
 recognition = sr.Recognizer()
@@ -63,8 +62,8 @@ def recognize(selection,number):
             audio = recognition.listen(source,timeout=3)
             try:
                 voice_data = recognition.recognize_google(audio)
-                print(voice_data)
-                exec_command(voice_data)
+                print(voice_data.lower())
+                exec_command(voice_data.lower())
             except sr.UnknownValueError:
                 engine.say('Sorry, I did not get that')
             except sr.RequestError:
