cb10230392ad5427cc9383c8773ca1dde3d36a98
NILANJAN MAJUMDAR <n.majumder14@gmail.com>
2020-10-29 13:44:26 +0530

    committed by viki the gitbot

--- demo_reverse_search.py
+++
@@ -1,5 +0,0 @@
-
-test_dict = { "what is your name" :["what's your name","tell me your name"],
-              "time" : ['time','whats the time'],
-              "backup my code" : ['backup',"git add all"]}
-
